10ab17ed3f02d775b7deacc72fe1206373a63d0d
EduardGazaryan <eduardgazaryan@gmail.com>
2025-02-17 13:03:11 +0300

    17_2

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+
 let currentPage = 1;
 const totalPages = document.querySelectorAll('.page').length;
+
+if (location.hash) {
+    currentPage = Number(location.hash.slice(1));
+}
 
 document.getElementById('page' + currentPage).classList.add('active');
 
@@ -18,5 +23,34 @@ function previousPage() {
         document.getElementById('page' + currentPage).classList.add('active');
     }
 }
+
+function goToPage(pageNumber) {
+    currentPage = parseInt(pageNumber);
+    history.pushState(null, null, '#' + pageNumber);
+    updateActivePage();
+}
+
+function updateActivePage() {
+    const pages = document.querySelectorAll('.page');
+
+    pages.forEach((page) => {
+        if (page.classList.contains('active')) {
+            page.classList.remove('active');
+        }
+    });
+
+    const selectedPage = document.getElementById('page' + currentPage);
+    if (selectedPage) {
+        selectedPage.classList.add('active');
+    }
+}
+
+window.onload = function () {
+    updateActivePage();
+};
+
+
+
+
 
 
